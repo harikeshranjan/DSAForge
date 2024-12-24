1d1179b96e2e8ddd4a353a0d15fbe8bf277da04d
harikeshranjan <harikeshranjansinha14@gmail.com>
2024-12-24 15:43:22 +0000

    Added footer

---
+++ client/components/footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <footer className="py-5 border-t">
+      <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="/" className="hover:underline">DSAForge™</a>. All Rights Reserved.</span>
+    </footer>
+  )
+}
+
